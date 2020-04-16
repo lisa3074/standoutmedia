8d325eebdfbcbbe542a5ba60e709cd80e87e464d
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-04-16 19:33:22 +0200

    edit af php file

--- mail.php
+++ mail.php
@@ -1,10 +1,10 @@
 <?php
-$name = $_POST['firstname'];
+$name = $_POST['name'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
 $message = $_POST['message'];
 $formcontent ="Fra: $name \n Email: $email \n Besked: $message";
-$recipient ="lisabianca@mme.com";
+$recipient ="iam@frontnd.ninja";
 $subject ="contact form";
 $mailheader = "Fra: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
@@ -14,21 +14,19 @@ echo "Tusind tak for din besked. Vi vender tilbage!";
 
 <style>
     body {
-        background-color: #FCF9F6;
+        background-color: #fff;
         text-align: center;
         font-family: sans-serif;
         font-size: 0.9rem;
-        color: #403F3F;
+        color: #000;
     }
 
     .tibaws {
         height: 35px;
         width: 120px;
         text-align: center;
-        background-color: #FCF9F6;
+        background-color: #7a0202;
         top: 1.9rem;
-        border-radius: 10px;
-        border: 1px solid #FCF9F6;
         box-shadow: 0px 1px 5px 1px #aaaaaa;
         right: 12rem;
         z-index: 10;
@@ -40,7 +38,7 @@ echo "Tusind tak for din besked. Vi vender tilbage!";
         text-decoration: none;
         font-family: sans-serif;
         font-size: 0.9rem;
-        color: rgba(64, 63, 63, 0.81);
+        color: #fff;
         padding: 0.6rem 0;
         margin: 0;
         font-weight: 400;
@@ -53,7 +51,7 @@ echo "Tusind tak for din besked. Vi vender tilbage!";
 </style>
 <br><br><br>
 <div class="wrap">
-    <a href="kontakt.html">
+    <a href="index.html">
         <div class="tibaws">
             <p class="tilbage">TILBAGE</p>
         </div>
